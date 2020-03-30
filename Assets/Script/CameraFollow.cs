28fd6efbcfb7344971a1d214b0ba93b9c412c5f1
Lydia Astrella <lydiaastrella@gmail.com>
2020-03-30 20:23:30 +0700

    camera movement

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	void LateUpdate () {
+
+		if (playerTransform.position.x > -23.438 && playerTransform.position.x < 23.418) 
+		{
+			Vector3 temp = transform.position;
+			temp.x = playerTransform.position.x;
+			transform.position = temp;
+		}
+	}
+}
