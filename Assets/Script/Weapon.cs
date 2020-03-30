000adf535820ed3f88ed6be77482a3541230a4b5
Lydia Astrella <lydiaastrella@gmail.com>
2020-03-30 22:48:54 +0700

    add shooting sound effect

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -7,11 +7,15 @@ public class Weapon : MonoBehaviour {
 	public Transform firePoint;
 	public GameObject bulletPrefab;
 	public Animator animator;
+	public AudioSource shootingAudio;
+	public AudioClip fireClip;
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
 			animator.SetBool ("isShooting", true);
+			shootingAudio.clip = fireClip;
+			shootingAudio.Play ();
 			Shoot ();
 			Invoke ("SetBoolBack", 0.5f);
 		}
