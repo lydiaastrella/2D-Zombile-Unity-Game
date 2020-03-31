8236bb943097e5e8cd194c087c20f0a78f83b272
Lydia Astrella <lydiaastrella@gmail.com>
2020-03-31 20:21:39 +0700

    post score

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour {
 		if (gameFinished && round <= 3) {
 			Spawn ();
 		} else if (gameFinished && round > 3) {
-			Debug.Log ("Game Finshed");
+			//Debug.Log ("Game Finshed");
 			player.SetActive (false);
 			updateScoreboard.SetActive (true);
 		} else if (!gameFinished) {
@@ -70,6 +70,37 @@ public class GameManager : MonoBehaviour {
 
 	public void updateScore(){
 		//update score
-		SceneManager.LoadScene("Menu");
+		string json = "{'username':'" + username.text.ToString() + "','score':" + scoreText.text.ToString() + "}";
+		Debug.Log (json);
+
+		string URL = "134.209.97.218:5051/scoreboards/13517019";
+
+		Dictionary<string,string> parameters = new Dictionary<string,string> ();
+		parameters.Add ("Content-Type", "application/json");
+		parameters.Add ("Content-Length", json.Length.ToString ());
+
+		json = json.Replace ("'", "\"");
+
+		Debug.Log (json);
+
+		byte[] postData = System.Text.Encoding.UTF8.GetBytes (json);
+
+		WWW www = new WWW (URL, postData, parameters);
+
+		StartCoroutine (WaitForRequest (www));
+		Debug.Log ("udh start coroutine");
+	}
+
+	IEnumerator WaitForRequest(WWW www){
+		Debug.Log("udh wait for request");
+		yield return www;
+		if (www.error == null) {
+			Debug.Log ("success");
+			Debug.Log (www.text);
+		} else {
+			Debug.Log ("error");
+			Debug.Log (www.error);
+		}
+		SceneManager.LoadScene ("Menu");
 	}
 }
